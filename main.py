e90ad0bd7588ccb28b475f11181f0e65303cc147
Mariohenriquecmv <mariohcmv@gmail.com>
2025-01-23 16:01:06 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+
+import pandas as pd
+#import pyautogui as pa
+# EU QUERO RECEBER UM ARQUIVO .CSV
+
+# QUERO AUTOMATIZAR o cadastro destes produtos no meu site
+# o arquivo csv deverá estar dentro da mesma pasta
+
+
+df = pd.read_csv("produtos.csv")
+
+print(df)
+
+#for coluna in df:
+#    print(coluna)
